d15af0c3dcc93be2a0dae580e2caf5cf77fbd68a
Vincenzo <vincebshock@gmail.com>
2021-03-12 23:27:10 +0100

    ✨UserContext injected

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import { Router } from '@reach/router';
 import PlayerPage from './pages/player/PlayerPage';
 import SignInPage from './pages/auth/signIn/SignInPage';
 import SignUpPage from './pages/auth/signUp/SignUpPage';
+
+import UserContextProvider from './contexts/UserContext';
+
 import './App.css';
 
 function App() {
   const user = null;
   return user ? (
-    <PlayerPage />
+    <UserContextProvider>
+      <PlayerPage />
+    </UserContextProvider>
   ) : (
-    <Router>
-      <SignUpPage path="signUp" />
-      <SignInPage path="/" />
-    </Router>
+    <UserContextProvider>
+      <Router>
+        <SignUpPage path="signUp" />
+        <SignInPage path="/" />
+      </Router>
+    </UserContextProvider>
   );
 }
 
