0ee4624e656ec4a3626eaa96f9fa2b7d3cc62999
Vincenzo <vincebshock@gmail.com>
2021-03-17 14:10:39 +0100

    ✨remove icon and animation added

--- src/components/audiobook-preview/AudiobookPreview.js
+++ src/components/audiobook-preview/AudiobookPreview.js
@@ -2,19 +2,45 @@ import React, { Component } from 'react';
 import './AudiobookPreview.css';
 
 class AudiobookPreview extends Component {
-  state = {};
+  state = {
+    holdTimer: null,
+    deleteEnabled: false,
+  };
   componentDidMount() {
     console.log(this.props.selected);
   }
+  handleMouseDown = (e) => {
+    this.setState({
+      holdTimer: setTimeout(() => {
+        if (!this.props.selected)
+          this.setState({ deleteEnabled: !this.state.deleteEnabled });
+      }, 1000),
+    });
+  };
+  handleMouseUp = (e) => {
+    clearTimeout(this.state.holdTimer);
+    if (!this.state.deleteEnabled) this.props.onClicked();
+  };
   render() {
     const { coverImage, title, author, selected } = this.props;
     return (
       <div
-        className={`audiobook-preview ${selected ? 'selected' : ''}`}
-        onClick={this.props.onClicked}
+        className={`audiobook-preview ${selected ? 'selected' : ''} ${
+          this.state.deleteEnabled ? 'edit-enabled' : ''
+        }`}
+        onMouseDown={(e) => this.handleMouseDown(e)}
+        onMouseUp={(e) => this.handleMouseUp(e)}
       >
         <div className="container">
           <img src={coverImage} alt="" className="cover" />
+          <div
+            className={`remove-button ${
+              this.state.deleteEnabled ? 'visible' : ''
+            }`}
+            onClick={(e) =>
+              this.props.onAudiobookRemove(this.props.audiobookID)
+            }
+          />
           <div className="text">
             <div className="title">{title}</div>
             <div className="author">{author}</div>
--- src/pages/player/PlayerPage.js
+++ src/pages/player/PlayerPage.js
@@ -48,6 +48,13 @@ class PlayerPage extends Component {
       })
       .catch((err) => console.log(err));
   };
+  onAudiobookRemove = (audiobookID) => {
+    console.log(audiobookID);
+    this.context.deleteAudiobook(audiobookID).then((res) => {
+      console.log(res);
+      this.context.refreshContext();
+    });
+  };
   render() {
     const menuItem = this.context?.user.isAdmin ? (
       <MenuItem>
@@ -111,12 +118,16 @@ class PlayerPage extends Component {
                     coverImage={audiobook.coverImage}
                     title={audiobook.title}
                     author={audiobook.author}
+                    audiobookID={audiobook._id}
                     selected={
                       this.context.selectedAudiobook._id === audiobook._id
                     }
                     onClicked={(e) => {
                       this.handleABookSelection(audiobook);
                     }}
+                    onAudiobookRemove={(audiobookID) =>
+                      this.onAudiobookRemove(audiobookID)
+                    }
                     key={key}
                   />
                 );
